8840078506a73c4e5d3a2eeb5680ef14784ad590
jay-mm-message <jay.mm.message@gmail.com>
2024-10-17 07:02:40 +0800

    feature-sum first commit

--- hello.c
+++ hello.c
@@ -7,8 +7,17 @@ void loopPrint(int times) {
         --times;
     } 
 }
+
+int sum(int a, int b) {
+    return a+b;
+}
+
 int main(void){
     printf("good day\n");
     loopPrint(5);
+    int a = 100;
+    int b = 200;
+    int sum_result = sum(a, b);
+    printf("a = %d, b = %d , sum = %d\n", a, b, sum_result);
     return 0;
 }
