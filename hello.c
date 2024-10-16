f694876c4c613f26de91fd951503b864ee90eb38
jay-mm-message <jay.mm.message@gmail.com>
2024-10-17 06:52:15 +0800

    feature-loop_print first commit

--- hello.c
+++ hello.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void loopPrint(int times) {
+    while(times > 0) {
+        printf("times: %d\n", times);
+        --times;
+    } 
+}
 int main(void){
     printf("good day\n");
+    loopPrint(5);
     return 0;
 }
